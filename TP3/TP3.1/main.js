770b54edc9f95a2f9f03acfffed05277c11304db
Jose <zeferreira1458@hotmail.com>
2023-12-13 15:08:48 +0100

    update main.js tp3.1

--- TP3/TP3.1/main.js
+++ TP3/TP3.1/main.js
@@ -2,17 +2,15 @@ document.querySelector('#get-citation').addEventListener('click', (e) => {
     fetch('https://kaamelott.reiter.tf/quote/random').then(response => response.json()).then(data => {
         console.log(data)
 
-        const citation = data.citation;
-        const personnage = data.infos.personnage;
-        const customerTable = document.querySelector('#afficherCitation tbody');
+        const citationTable = document.querySelector('#afficherCitation tbody');
         const newRow = document.createElement('tr');
 
         newRow.innerHTML = `
-                <p>Citation: "${citation}"</p>
-                <p>Personnage: ${personnage}</p>
+                <p>Citation: "${data.citation}"</p>
+                <p>Personnage: ${data.infos.personnage}</p>
             `;
 
-        customerTable.appendChild(newRow);
+        citationTable.appendChild(newRow);
     })
 });
 
